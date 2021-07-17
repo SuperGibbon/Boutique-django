1129ab8ef512c3bfae53e405c297cdac2516a620
SuperGibbon <super_gibbon@hotmail.com>
2021-07-17 09:31:08 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
 
 def view_bag(request):
-    """ A view to see shopping bag """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
@@ -12,6 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -32,13 +36,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """ adjust quantity of the specified product to the shopping bag """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -47,9 +52,8 @@ def adjust_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
-            if quantity > 0:
-                bag[item_id]['items_by_size'][size] = quantity
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
@@ -65,7 +69,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ adjust quantity of the specified product to the shopping bag """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -74,11 +78,9 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         if size:
-            if item_id in list(bag.keys()):
-                del bag[item_id]['items_by_size'][size]
-                if not bag[item_id]['items_by_size']:
-                    bag.pop(item_id)
-
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
         else:
             bag.pop(item_id)
 
